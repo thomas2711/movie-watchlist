ae5ad197854fe03baba04710b08cacf2815058c2
thomasedw <ter1@williams.edu>
2017-08-18 19:02:34 -0400

    Adjusted function name

--- rt.py
+++ rt.py
@@ -7,15 +7,15 @@ rt_prefix = "https://www.rottentomatoes.com/m/"
 def get_rt_score(title, year):
     rt_url_a = rt_prefix + title.replace(' ','_').replace("'", '').replace(':','').replace('.','').lower()
 
-    rt_rating = rt_score_helper(title, year, rt_url_a)
+    rt_rating = rt_score_from_url(rt_url_a)
     if rt_rating != -1:
         return rt_rating
     else:
         rt_url_b = rt_url_a + '_' + str(year)
-        rt_rating = rt_score_helper(title, year, rt_url_b)
+        rt_rating = rt_score_from_url(rt_url_b)
         return rt_rating 
 
-def rt_score_helper(title, year, url):
+def rt_score_from_url(url):
     page = requests.get(url).text
     soup = BeautifulSoup(page, 'html.parser')
     #print(soup.prettify())  
